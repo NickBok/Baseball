009723e38b849ee96fd77e453d4f3bb85ca98ae4
Declan Costello <dec1costello@gmail.com>
2023-09-03 11:54:26 -0600

    Update streamlit_app.py

--- Distance-Predictor/streamlit_app.py
+++ Distance-Predictor/streamlit_app.py
@@ -3,18 +3,17 @@ import pandas as pd
 import pickle
 import numpy as np
 
-
 st.title('Predicted Distance is...')
 
-la = st.slider("Launch Angle",0,50, value=25)
-ev = st.slider("Exit Velocity",0,120, value=60)
-pull_percent =  st.slider("Pull %",0.0,1.0, value = 0.5)
+#la = st.slider("Launch Angle",0,50, value=25)
+#ev = st.slider("Exit Velocity",0,120, value=60)
+#pull_percent =  st.slider("Pull %",0.0,1.0, value = 0.5)
 
 df = pd.read_csv('Distance-Predictor/sample_input.csv')
 
-df['launch_angle'].iloc[0] = la
-df['launch_speed'].iloc[0] = ev
-df['pull_percent'].iloc[0] = pull_percent
+df['launch_angle'].iloc[0] = st.slider("Launch Angle",0,60, value=30)
+df['launch_speed'].iloc[0] = st.slider("Exit Velocity",60,120, value=90)
+df['pull_percent'].iloc[0] = st.slider("Pull %",0.0,1.0, value = 0.5)
 
 pickled_model = pickle.load(open('Distance-Predictor/light_weight_model.pkl', 'rb'))
 pred = pickled_model.predict(df)
