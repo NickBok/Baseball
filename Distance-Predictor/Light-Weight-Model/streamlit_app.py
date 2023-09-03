57bc5dd26425b66dd95e2d3ec44438ca7fbb725e
Declan Costello <dec1costello@gmail.com>
2023-09-03 15:08:15 -0500

    Update streamlit_app.py

--- Distance-Predictor/Light-Weight-Model/streamlit_app.py
+++ Distance-Predictor/Light-Weight-Model/streamlit_app.py
@@ -4,16 +4,13 @@ import pickle
 
 st.title('Predicted Distance is...')
 
-df = pd.read_csv('Distance-Predictor/sample_input.csv')
+df = pd.read_csv('Distance-Predictor/Light-Weight-Model/sample_input.csv')
 
 df['launch_angle'].iloc[0] = st.slider("Launch Angle",0,60, value=30)
 df['launch_speed'].iloc[0] = st.slider("Exit Velocity",60,120, value=90)
 df['pull_percent'].iloc[0] = st.slider("Pull %",0.0,1.0, value = 0.5)
 
-pickled_model = pickle.load(open('Distance-Predictor/lw_model.pkl', 'rb'))
+pickled_model = pickle.load(open('Distance-Predictor/Light-Weight-Model/lw_model.pkl', 'rb'))
 pred = pickled_model.predict(df)
 
 st.title(pred[0])
-
-#once extra folder throw this link in to show under predicting
-#https://twitter.com/homerunreport?s=20
