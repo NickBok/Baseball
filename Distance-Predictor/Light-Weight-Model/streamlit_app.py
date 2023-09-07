b176a22923baf6768fe28dc17bbc7cf8dc5ec1b9
Declan Costello <dec1costello@gmail.com>
2023-09-07 17:55:15 -0600

    Update streamlit_app.py

--- Distance-Predictor/Light-Weight-Model/streamlit_app.py
+++ Distance-Predictor/Light-Weight-Model/streamlit_app.py
@@ -28,7 +28,13 @@ tab_ppredictor, tab_faq = st.tabs(["The Distance Predictor", "FAQ"])
 
 with tab_ppredictor:
 
-            st.markdown('''#### The Predicted Distance is...''', unsafe_allow_html=True)
+        
+            res = pred[0]
+            string = 'The Predicted Distance is... '
+
+            final = (string + str(res))
+            #st.markdown('''#### The Predicted Distance is...''' + str(res), unsafe_allow_html=True)
+            st.markdown(final)
 
             df = pd.read_csv('Distance-Predictor/Light-Weight-Model/sample_input.csv')
             
@@ -39,7 +45,7 @@ with tab_ppredictor:
             pickled_model = pickle.load(open('Distance-Predictor/Light-Weight-Model/lw_model.pkl', 'rb'))
             pred = pickled_model.predict(df)
             
-            st.title(str(pred[0]))
+            #st.title(str(pred[0]))
 
 with tab_faq:
             st.markdown(" ### Frequently Asked Questions 🔎 ")
