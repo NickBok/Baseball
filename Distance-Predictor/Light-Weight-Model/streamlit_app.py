0d0af661b77a76d9e2106a53ea0e91a59edfd364
Declan Costello <dec1costello@gmail.com>
2023-09-07 19:53:03 -0600

    Update streamlit_app.py

--- Distance-Predictor/Light-Weight-Model/streamlit_app.py
+++ Distance-Predictor/Light-Weight-Model/streamlit_app.py
@@ -61,7 +61,7 @@ with tab_faq:
             expand_faq3 = st.expander(":baseball: How was the predictive model trained?", expanded=False)
             with expand_faq3:
                 
-                st.write('''To train my model, I collected data from [Pybaseball](https://github.com/jldbc/pybaseball), specifically from the 2022 MLB Season. For this specific Streamlit Model I only used the EV, LA, & Pull% as training features, but feel free to check out my [full model](https://github.com/dec1costello/Baseball/tree/main/Distance-Predictor) where I use more existing features and make my own as well 🧪 ''')
+                st.write('''To train my model, I collected data from [Pybaseball](https://github.com/jldbc/pybaseball), specifically from the 2022 MLB Season. For this specific Streamlit Model I only used the EV, LA, & Pull% as training features, but feel free to check out my [full model](https://github.com/dec1costello/Baseball/tree/main/Distance-Predictor) where I use more existing features and make my own as well 🧪 👨‍🔬 🧬!''')
 
             expand_faq4 = st.expander(":baseball: How can the model imporve?", expanded=False)
             with expand_faq4:
