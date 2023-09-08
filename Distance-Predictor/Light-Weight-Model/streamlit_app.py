3dd7d8e1c167cc518502a4d70087394ff4cd7c9a
Declan Costello <dec1costello@gmail.com>
2023-09-07 19:45:28 -0600

    Update streamlit_app.py

--- Distance-Predictor/Light-Weight-Model/streamlit_app.py
+++ Distance-Predictor/Light-Weight-Model/streamlit_app.py
@@ -55,7 +55,7 @@ with tab_faq:
             expand_faq2 = st.expander(":baseball: What machine learning model did you use?")
             with expand_faq2:    
                 
-                st.write('''I tried various regression, classification, and hybrid approaches and found that using  a Random Forest Classifier as my predictive model gave accurate and meaningful results. A Random Forest is an ensemble model consisting of thousands of Decision Trees, with each tree constructed from a random bootstrapped sample of players in the training set; each node on each tree is split using a random sample of the feature (input) variables. The values of hyperparameters such as maximum tree depth and  number of features considered at each node were arrived at via grid search optimization.''')
+                st.write('''I tried various regression models and found that using an XGB regressor as my predictive model gave accurate and meaningful results. An XGBRegressor is a machine learning model that uses the gradient boosting algorithm to predict continuous numerical values. XGBoost stands for eXtreme Gradient Boosting and is very precise.''')
                 st.image('https://github.com/dec1costello/Baseball/assets/79241861/11a4414a-7b01-4f05-9625-90a3de21c752')
             
             expand_faq3 = st.expander(":baseball: How was the predictive model trained?", expanded=False)
